98f20644b63a5068753f379abc875fbe7b7553a5
rkant225 <rkant225@gmail.com>
2020-05-02 02:08:09 +0530

    Updates

---
+++ precache-manifest.0e9fa3d47a569d3fccc61d771cf175df.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e322d160d101273a68aca84bc9847cd",
+    "url": "/COVID_19_tracker_WorldWide/index.html"
+  },
+  {
+    "revision": "b51b6c0ebc2f09aee8da",
+    "url": "/COVID_19_tracker_WorldWide/static/css/main.14afc888.chunk.css"
+  },
+  {
+    "revision": "90179b917f6e22e42776",
+    "url": "/COVID_19_tracker_WorldWide/static/js/2.7bb7697a.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/COVID_19_tracker_WorldWide/static/js/2.7bb7697a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b51b6c0ebc2f09aee8da",
+    "url": "/COVID_19_tracker_WorldWide/static/js/main.f4967552.chunk.js"
+  },
+  {
+    "revision": "b82c36f5de74c0dd7705",
+    "url": "/COVID_19_tracker_WorldWide/static/js/runtime-main.f975d4d6.js"
+  },
+  {
+    "revision": "a691af5e0e1d378bab64195ae7b6e2d9",
+    "url": "/COVID_19_tracker_WorldWide/static/media/logo.a691af5e.jpg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/COVID_19_tracker_WorldWide/precache-manifest.0e9fa3d47a569d3fccc61d771cf175df.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/COVID_19_tracker_WorldWide/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
